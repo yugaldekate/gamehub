f0a0d771e443557c0f09db2d905828871a34a77f
Yugal D Dekate <yugaldekate72@gmail.com>
2024-01-24 19:54:39 +0530

    created the dashboard's sidebar and navbar

--- app/(browse)/_components/sidebar/user-item.tsx
+++ app/(browse)/_components/sidebar/user-item.tsx
@@ -52,7 +52,7 @@ export const UserItemSkeleton = () => {
     return (
         <li className="flex items-center gap-x-4 px-3 py-2">
             <Skeleton className="min-h-[32px] min-w-[32px] rounded-full" />
-            <div className="flex-1">
+            <div className="flex-1 hidden lg:block">
                 <Skeleton className="h-6" />
             </div>
         </li>
